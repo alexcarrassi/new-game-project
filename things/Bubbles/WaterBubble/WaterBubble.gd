fa96bb6f371ba8744c3179073cf42da9228505a6
Alex <alex@arkdesign.nl>
2026-01-15 15:42:53 +0100

    Using CharacterBody2D for the Water Projectile

--- things/Bubbles/WaterBubble/WaterBubble.gd
+++ things/Bubbles/WaterBubble/WaterBubble.gd
@@ -1,33 +1,14 @@
 class_name WaterBubble extends Bubble
 
-@export var tideSpeed: float = 150
-@onready var tidePath: Path2D = $Path2D
-@onready var tidePathFollow: PathFollow2D = $Path2D/PathFollow2D
-
-@onready var groundCast: ShapeCast2D = $Sensors/GroundCast
-@onready var frontCast: RayCast2D = $Sensors/FrontCast
-
-var sprite_flicker_hz = 45.0
-var isGrounded: bool = false
-
-var _flicker_accum: float = 0.0
-
+@export var projectile: PackedScene 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	super._ready()
 	self.setFloating()
-	self.tidePathFollow.visible = false
 
-	
-	#self.tidePath.set_as_top_level(true)
-	self.body_entered.connect( func(body: Node) -> void:
-		print(body.name)	
-	)
-	
-	self.tidePath.curve = Curve2D.new()
 	pass # Replace with function body.
-	
+
 
 #We skip the popping state, and go straight into the Bubble's Shoot state
 func playerPop(player: Player) -> void:
@@ -39,21 +20,13 @@ func playerPop(player: Player) -> void:
 	self.animationPlayer.play("TIDE")
 	self.target_velocity = Vector2.ZERO
 	self.linear_velocity = Vector2.ZERO
-	#self.tidePath.reparent(Game.world.level)
-	self.tidePath.curve.add_point(self.position)
-
-	self.set_collision_layer_value(1, false)
 	
+	var projectile: WaterProjectile = self.projectile.instantiate()
+	self.get_parent().add_child(projectile)
+	projectile.position = self.position
+	self.queue_free()
 	pass
 	
-func tryAddPathPoint(point: Vector2) -> void:
-	var pointCount = self.tidePath.curve.point_count
-	self.tidePath.curve.add_point(point, Vector2.ZERO, Vector2.ZERO, clamp(pointCount - 2, 0, pointCount - 1) )	
-	
-func setPathHead(point: Vector2) -> void:
-	var point_count = self.tidePath.curve.point_count
-	self.tidePath.curve.add_point( point )
-		
 
 
 
@@ -67,60 +40,3 @@ func _physics_process(delta: float) -> void:
 			
 			
 			self.hurtbox_update(delta)
-	
-		BubbleState.Shooting:
-			
-			
-			# if grounded, horizontal
-			# not grounded, fall down	
-			if(self.isGrounded):
-				self.target_velocity = Vector2(self.tideSpeed * self.dir.x, self.get_gravity().y * delta)
-			else:
-				self.target_velocity =  Vector2(0, self.tideSpeed)
-				
-			self.linear_velocity = self.target_velocity	
-				
-			self.setPathHead( self.position )
-
-
-			#If we're switching states, we need to record that position for the sprite's oscillation	
-			if(self.groundCast.is_colliding() != self.isGrounded):
-				self.tryAddPathPoint( self.position)
-			
-			# We were airborne just now, but landing
-			if(!self.isGrounded && self.groundCast.is_colliding()):
-				self.dir.x *= -1
-				self.frontCast.target_position.x *= -1
-				
-			# We were grounded and have just hit a wall	
-			#if(self.isGrounded && self.frontCast.is_colliding()):
-				#self.queue_free()
-
-			self.isGrounded = self.groundCast.is_colliding()			
-	
-	
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	
-	#self.tidePathFollow.progress += self.tideSpeed * 3 * delta
-	if(self.state == BubbleState.Shooting):
-		
-		self._flicker_accum += delta
-		if(self._flicker_accum >= 1/self.sprite_flicker_hz) :
-			self._flicker_accum = 0
-			self.tidePathFollow.visible = !self.tidePathFollow.visible 
-			self.sprite.visible = !self.tidePathFollow.visible
-			
-		
-		var curve_length = self.tidePath.curve.get_baked_length()
-		if(curve_length < 32):
-			self.tidePathFollow.progress = 0
-		
-		else:
-
-			#var progress_ofsset = clamp(curve_length - 32, 0, curve_length  -32)
-			self.tidePathFollow.progress +=  self.tideSpeed*delta
-			
-		if( curve_length > 0 && self.tidePathFollow.progress == curve_length ) :
-			self.queue_free()
---
+++ things/Bubbles/WaterBubble/water_projectile.gd
@@ -0,0 +1,93 @@
+class_name WaterProjectile extends CharacterBody2D
+
+
+
+@export var tideSpeed: float = 210.0
+@onready var tidePath: Path2D = $Path2D
+@onready var tidePathFollow: PathFollow2D = $Path2D/PathFollow2D
+@onready var hitBox: Area2D = $Hitbox
+@onready var sprite: Sprite2D = $Sprite2D
+
+var dir: Vector2 = Vector2.RIGHT
+var projectileSpeed: float = 220.0
+
+var sprite_flicker_hz = 30.0
+var isGrounded: bool = false
+
+
+var _flicker_accum: float = 0.0
+
+func _ready() -> void:	
+	self.tidePath.set_as_top_level(true)
+	self.tidePath.curve = Curve2D.new()
+	self.hitBox.area_entered.connect( self.hitBoxAreaEntered)
+
+
+func hitBoxAreaEntered(area: Area2D) -> void:
+	var areaOwner = area.get_parent()
+	if areaOwner is Enemy:
+		areaOwner.sm_locomotion.state.finished.emit("FALLING")
+		areaOwner.sm_status.state.finished.emit("DEAD", {"dir": self.dir.x})
+		areaOwner.rotation = 0
+
+
+	
+func tryAddPathPoint(point: Vector2) -> void:
+	var pointCount = self.tidePath.curve.point_count
+	self.tidePath.curve.add_point(point, Vector2.ZERO, Vector2.ZERO, clamp(pointCount - 2, 0, pointCount - 1) )	
+	
+func setPathHead(point: Vector2) -> void:
+	var point_count = self.tidePath.curve.point_count
+	self.tidePath.curve.add_point( point )
+		
+
+	
+	
+func _physics_process(delta: float) -> void:
+	
+	# Add the gravity.
+	var newGround = self.is_on_floor()
+	if not is_on_floor():
+		self.velocity.y = self.projectileSpeed
+		self.velocity.x = 0
+	else :
+		self.velocity.x = self.dir.x * self.projectileSpeed
+
+	move_and_slide()
+	
+
+	#If we're switching states, we need to record that position for the sprite's oscillation	
+	if(self.is_on_floor() && !self.isGrounded):
+		self.dir.x *= -1
+
+	else:
+		self.setPathHead( self.position )
+		
+		
+	if(newGround != self.isGrounded):
+		self.tryAddPathPoint( self.position)
+	
+	self.isGrounded = self.is_on_floor()
+
+func _process(delta: float) -> void:
+
+	self._flicker_accum += delta
+	if(self._flicker_accum >= 1/self.sprite_flicker_hz) :
+		self._flicker_accum = 0
+		self.tidePathFollow.visible = !self.tidePathFollow.visible 
+		self.sprite.visible = !self.tidePathFollow.visible
+		
+	
+	var curve_length = self.tidePath.curve.get_baked_length()
+	if(curve_length < 32):
+		self.tidePathFollow.progress = 0
+	
+	else:
+
+		#var progress_ofsset = clamp(curve_length - 32, 0, curve_length  -32)
+		self.tidePathFollow.progress +=  self.tideSpeed*delta
+		
+	if( curve_length > 0 && self.tidePathFollow.progress == curve_length ) :
+		self.queue_free()
+
+		
