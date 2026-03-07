bcecee17ab95f8a9c310cb1f5229bc4a988cd0e8
Alex <alex@arkdesign.nl>
2026-03-07 17:51:17 +0100

    PlayerBubbles to Item Effect created

---
+++ things/Inventory/ItemEffects/PlayerBubblesToItems.gd
@@ -0,0 +1,24 @@
+class_name PlayerBubblesToItems extends ItemEffect
+
+@export var item: Item
+
+func apply( ctx: ItemUseContext ) -> void:
+
+
+	if(item) :
+		
+		var bubbles = Game.world.get_tree().get_nodes_in_group("PlayerBubble")
+		for bubble: PlayerBubble in bubbles:
+			var itemPickup: ItemPickup
+			
+			if(item.itemPickup != null):
+				itemPickup = item.itemPickup.instantiate() as ItemPickup
+			else:
+				itemPickup = ItemDB.default_item_pickup.instantiate( ) as ItemPickup
+		
+			itemPickup.position = bubble.position
+			itemPickup.item = item
+			Game.world.level.add_child(itemPickup)
+			bubble.queue_free()
+			print(bubbles)
+	
